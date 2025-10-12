ea3d84ff86169fbce0c92d02b81b4f7ce7820191
Satish Singh <i.sksingh113@gmail.com>
2025-10-12 18:58:39 +0530

    test

--- services/cookieServices.js
+++ services/cookieServices.js
@@ -3,8 +3,8 @@ const setTokenCookie = (res, token) => {
   res.setHeader("Set-Cookie", [
     serialize("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
     }),
@@ -16,8 +16,8 @@ const clearTokenCookie = (res) => {
     serialize("token", "", {
 
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       expires: new Date(0),
     }),
