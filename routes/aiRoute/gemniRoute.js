28f4a1a85f634ac759e76b7bbafc9607cb410226
Satish Singh <i.sksingh113@gmail.com>
2025-10-31 17:10:47 +0530

    ai integrated

--- routes/aiRoute/gemniRoute.js
+++ routes/aiRoute/gemniRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { improveText } = require("../../controller/aiController/gemni");
 const router = express.Router();
-
+//hello
 router.post("/improve-text", improveText);
 
 module.exports = router;
